afca040515881495a67a58642606db60e5f21bd0
Afonin Sergey <afoninsergey@mail.ru>
2025-04-11 21:17:06 +0300

    feat: Реализует переключение звуков и обои + кнопка паузы

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
 import './sass/index.sass';
 
-console.log('Webpack');
+const buttons = document.querySelectorAll('button');
+const sounds = document.querySelectorAll('audio');
 
+buttons.forEach((btn) => {
+	btn.addEventListener('click', ({ currentTarget }) => {
+		buttons.forEach(b => {if (b !== currentTarget) b.classList.remove('pause')})
+
+		document.body.className = '';
+
+		currentTarget.classList.toggle('pause')
+
+		const buttonClassName = currentTarget.className;
+	
+		if (!buttonClassName.includes('sun')) {
+			document.body.classList.add(
+				`${buttonClassName.includes('rain') ? 'rainy' : 'winter'}`
+			);
+		}
+
+		sounds.forEach((sound) => {
+			sound.pause();
+		
+			if (buttonClassName.includes(sound.id)) {
+				sound.classList.toggle('play');
+				if (sound.classList.contains('play')) {
+					sound.play();
+				}
+			} else {
+				sound.className = '';
+			}
+		});
+	});
+});
