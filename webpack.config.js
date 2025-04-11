0c9028b6714cb548dab6f57edce71909d0db76e5
Afonin Sergey <afoninsergey@mail.ru>
2025-04-11 16:05:21 +0300

    chore: Подключает шрифты, картинки, добавляет аудио, немного оптимизирует картинки

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
@@ -10,16 +12,33 @@ module.exports = {
 	output: {
 		filename: '[name].[contenthash].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-		clean: true
+		clean: true,
+		assetModuleFilename: 'images/[name].[hash][ext][query]'
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, 'public/index.html')
 		}),
-		new MiniCssExtractPlugin()
+		new MiniCssExtractPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'public/favicon.png'),
+					to: path.resolve(__dirname, 'dist')
+				},
+				{
+					from: path.resolve(__dirname, 'src/assets/sounds'),
+					to: path.resolve(__dirname, 'dist/sounds')
+				},
+				{
+					from: path.resolve(__dirname, 'src/assets/icons'),
+					to: path.resolve(__dirname, 'dist/icons')
+				}
+			]
+		})
 	],
 	module: {
-		rules: [	
+		rules: [
 			{
 				test: /\.s[ac]ss$/i,
 				use: [
@@ -36,10 +55,25 @@ module.exports = {
 					'sass-loader'
 				]
 			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource'
+			},
 		]
 	},
 	optimization: {
-		minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+		minimizer: [
+			new CssMinimizerPlugin(),
+			new TerserPlugin(),
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.imageminMinify,
+					options: {
+						plugins: [['mozjpeg', { quality: 70 }]]
+					}
+				}
+			})
+		],
 		minimize: true
 	}
 };
